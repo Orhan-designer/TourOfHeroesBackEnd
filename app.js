133608697751f8b9b4b120d0e2067df8820e67bc
Orhan <ya.eskimo-callboy@yandex.by>
2022-02-10 21:31:20 +0300

    completely redone project, deleted all folders

--- app.js
+++ app.js
@@ -1,34 +1,124 @@
 const express = require("express");
 const app = express();
-const MongoClient = require('mongodb').MongoClient; //подключаем монгоДб
-const url = 'mongodb+srv://Orhan:Mamedov03Danskih09@cluster0.md34d.mongodb.net/TourOfHeroesDt?retryWrites=true&w=majority'; //подключаемся к базе данных в монгодб
-const api = require('./routes/api');
+const jsonParser = express.json;
+const MongoClient = require("mongodb").MongoClient; //подключаем монгоДб
+const url =
+  "mongodb+srv://Orhan:Mamedov03Danskih09@cluster0.md34d.mongodb.net/TourOfHeroes?retryWrites=true&w=majority"; //подключаемся к базе данных в монгодб
+const objectId = require("mongodb").ObjectId;
+
+let dbClient;
 
 const PORT = process.env.PORT || 3000;
 //Connect to DB
-MongoClient.connect(url, (err) => { //коннектимся через MongoClient, и добавляем второй параметр client
-    if (err) {
-        console.log('Connection error', err);
-        throw err;
-    }; 
+MongoClient.connect(url, (err, client) => {
+  //коннектимся через MongoClient, и добавляем второй параметр client
+  if (err) return console.error(err);
 
-    console.log('Connected to db');
+  dbClient = client;
+  app.locals.collection = client.db("TourOfHeroes").collection("heroes");
+  //How to we start listening to the server
+  app.listen(PORT, function () {
+    console.log("Server running on localhost:" + PORT);
+  });
 
+  console.log("Connected to db");
 });
-
-//Middleware
-app.use(express.json());
-app.use('/api', api);
-app.use('/api/heroes', api);
-app.use('/api/heroes/:id', api);
 
 //Our main page greetings
-app.get('/', (req, res) => {
-    res.send('Hello from Server');
+app.get("/", (req, res) => {
+  res.send("Hello from Server");
 });
 
-//How to we start listening to the server
-app.listen(PORT, function () {
-    console.log('Server running on localhost:' + PORT);
+app.get("/api/heroes", (req, res) => {
+
+  const collection = req.app.locals.collection;
+
+  collection.find({}).toArray((err, heroes) => {
+    if (err) return console.error(err);
+    res.send(heroes);
+  });
+
 });
 
+app.get("/api/heroes/:id", (req, res) => {
+
+    const id = req.params.id
+    console.log(id)
+    const collection = req.app.locals.collection;
+
+    collection.findOne({_id: id}, (err, hero) => {  
+        if(err) return console.log(err);
+        res.send(hero);
+    });
+});
+
+app.post("/api/heroes", jsonParser, (req, res) => {
+  if (!req.body) return res.sendStatus(400);
+
+  let hero = {
+    name: req.body.name,
+    age: req.body.userAge,
+    heroLevel: req.body.heroLevel,
+    alterEgo: req.body.alterEgo,
+    heroRace: req.body.heroRace,
+    heroClass: req.body.heroClass,
+    power: req.body.power,
+  };
+
+  const collection = req.app.locals.collection;
+
+  collection.insertOne(hero, (err, result) => {
+    if (err) return console.log(err);
+    res.send(hero);
+  });
+
+});
+
+app.delete("/api/heroes/:id", (req, res) => {
+  const id = new objectId(req.params.id);
+  const collection = req.app.locals.collection;
+
+  collection.findOneAndDelete({ _id: id }, (err, result) => {
+    if (err) return console.log(err);
+    let hero = result.value;
+    res.send(hero);
+  });
+
+});
+
+app.put("/api/heroes", jsonParser, (req, res) => {
+  if (!req.body) return res.sendStatus(400);
+
+  const id = new objectId(req.body.id);
+  const name = req.body.name;
+  const age = req.body.age;
+  const heroLevel = req.body.heroLevel;
+  const alterEgo = req.body.alterEgo;
+  const heroRace = req.body.heroRace;
+  const heroClass = req.body.heroClass;
+  const power = req.body.power;
+
+  const collection = req.app.locals.collection;
+
+  collection.findOneAndUpdate(
+    { _id: id },
+    {
+      $set: {
+        name: name,
+        age: age,
+        heroLevel: heroLevel,
+        alterEgo: alterEgo,
+        heroRace: heroRace,
+        heroClass: heroClass,
+        power: power,
+      },
+    },
+    { returnDocument: "after" },
+    (err, result) => {
+      if (err) return console.log(err);
+      const hero = result.value;
+      res.send(hero);
+    }
+  );
+
+});
